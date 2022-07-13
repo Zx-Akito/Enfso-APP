6ce97d98fccc692226b5727ef4791d78b714e267
Zx-Akito <zxakito89@gmail.com>
2022-07-13 14:55:40 +0700

    add component declaration

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { TrendingComponent } from './trending/trending.component';
+import { ContentsideComponent } from './contentside/contentside.component';
+import { DetailComponent } from './detail/detail.component';
 
 
 @NgModule({
   declarations: [
     DashboardComponent,
-    TrendingComponent
+    TrendingComponent,
+    ContentsideComponent,
+    DetailComponent
   ],
   imports: [
     CommonModule
