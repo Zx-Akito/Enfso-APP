8a5581ee93631658580bf89342516340877b4a13
Zx-Akito <zxakito89@gmail.com>
2022-07-13 14:56:58 +0700

    fix routing for sidebar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutsComponent } from './layouts/layouts.component';
+import { BidsComponent } from './pages/bids/bids.component';
+import { CreatorsComponent } from './pages/creators/creators.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SavedComponent } from './pages/saved/saved.component';
+import { WalletComponent } from './pages/wallet/wallet.component';
 
 const routes: Routes = [
   	{ 	
 		path: '', 
 		component: LayoutsComponent,
 		children: [
-			{path:'', component: DashboardComponent
-		}
+			{ path: '', redirectTo:'/dashboard', pathMatch: 'full' },
+			{ path: 'dashboard', component: DashboardComponent },
+			{ path:'bids', component:  BidsComponent},
+			{ path:'saved', component:  SavedComponent},
+			{ path:'creators', component:  CreatorsComponent},
+			{ path:'wallet', component:  WalletComponent}
+			
 		]
 	}
   
